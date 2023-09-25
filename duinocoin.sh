b8345137e2803df01437bea5f1ca4b96bd469a48
laissten31 <122177390+laissten31@users.noreply.github.com>
2023-09-25 14:26:31 +0200

    Update duinocoin.sh

--- duinocoin.sh
+++ duinocoin.sh
@@ -1,64 +1,38 @@
 #!/bin/bash
 
-# Vérifier la connectivité Internet avec un ping
-/bin/ping -c2 "1.1.1.1" >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo "Pas de connexion Internet."
-    exit 1
-fi
+# Votre code actuel pour la vérification du ping
 
-# Variables DuinoCoin
 TOKEN="5921647050:AAGOKwgPlM70CS0l2vEhaMycNlTuG5E6pQk"
 CHATID="5541683884"
 WALLET="hollix"
 
-# Fonction pour envoyer un message Telegram
-send_message() {
-    local text="$1"
-    curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage?parse_mode=HTML" \
-        -d "chat_id=$CHATID" \
-        -d "text=$text"
+api_telegram="https://api.telegram.org/bot$TOKEN/sendMessage?parse_mode=HTML"
+api_duinocoin=$(curl -s -X GET https://server.duinocoin.com/users/$WALLET -H "Accept: application/json" | jq .)
+
+BALANCE=$(echo $api_duinocoin | jq '.result.balance.balance')
+BALANCE_FORMAT=$(echo "scale=2; $BALANCE/1" | bc -l)
+WORKERS=$(echo $api_duinocoin | jq '.result.miners' | jq '.[].identifier' | tr -d '"')
+NUMBER_WORKERS=$(echo $api_duinocoin | jq -r '.result.miners' | jq -r '.[].identifier' | wc -l)
+
+function sendMessage() {
+    curl -s -X POST $api_telegram -d chat_id=$CHATID -d text="$(printf "<b>$BANNER</b>\n\n \
+        Balance: $BALANCE_FORMAT\n \
+        Nº Workers $NUMBER_WORKERS\n \
+        Names workers:\n<code>$WORKERS</code>")" >/dev/null 2>&1
 }
 
-# Fonction pour actualiser les données et envoyer le message
-refresh_data() {
-    # Appel à l'API DuinoCoin pour obtenir les données
-    api_duinocoin=$(curl -s -X GET "https://server.duinocoin.com/users/$WALLET" -H "Accept: application/json")
+# Gérez les commandes Telegram ici
+if [[ "$1" == "/start" ]]; then
+    # Code pour gérer la commande /start
+    BANNER="Le bot est prêt à fonctionner !"
+    sendMessage
+elif [[ "$1" == "/refresh" ]]; then
+    # Code pour gérer la commande /refresh
+    BANNER="ᕲ DuinoCoin ✅"  # Par exemple
+    sendMessage
+else
+    # Code pour vérifier le ping ou d'autres actions par défaut
+    # ...
 
-    # Extraction des données
-    BALANCE=$(jq -r '.result.balance.balance' <<< "$api_duinocoin")
-    WORKERS=$(jq -r '.result.miners[].identifier' <<< "$api_duinocoin")
-    NUMBER_WORKERS=$(jq -r '.result.miners | length' <<< "$api_duinocoin")
-
-    # Date et heure actuelles
-    current_time=$(date "+%H:%M:%S")
-
-    # Construction du message
-    message="<b>DuinoCoin Dashboard</b>
-
-Balance: $BALANCE
-Nº Workers: $NUMBER_WORKERS
-Workers: $WORKERS
-Date et heure actuelles: $current_time"
-
-    # Envoi du message Telegram
-    send_message "$message"
-}
-
-# Commande /start
-if [ "$1" == "/start" ]; then
-    echo "Bot prêt à fonctionner !"
-    exit 0
-fi
-
-# Commande /refresh
-if [ "$1" == "/refresh" ]; then
-    refresh_data
-    exit 0
-fi
-
-# Vérifier les arguments passés
-if [ $# -eq 0 ]; then
-    echo "Utilisation : $0 [/start|/refresh]"
-    exit 1
+    # Si vous ne spécifiez pas de commande Telegram, le script effectuera d'autres actions par défaut
 fi
